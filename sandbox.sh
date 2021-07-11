a37faefd16e830baa7c34153ebfe37a3c65a50f1
Michael Gilchrist <michaelgilch@gmail.com>
2021-07-11 06:46:10 -0400

    Sandbox - Active Window Dimensions

--- sandbox.sh
+++ sandbox.sh
@@ -4,12 +4,30 @@
 #
 # packages: wmctrl xdotool
 
+GetActiveWinGeo() {
+    wingeo1=$(xdotool getwindowgeometry $window)
+    echo "Active Window Geometry (xdotool) = $wingeo1"
+    echo ""
+
+    windowHex=$(echo "obase=16; 44042839" | bc | tr '[:upper:]' '[:lower:]')
+    wingeo2=$(wmctrl -liG | grep $windowHex)
+    echo "Active Window Geometry (wmctrl) = $wingeo2"
+    echo ""
+}
+
 desktopdims=$(wmctrl -d)
 echo "Desktop dimensions =" 
 echo "$desktopdims"
+echo ""
 
 window=$(xdotool getactivewindow)
 echo "Active Window = $window"
+echo ""
 
-echo " - moving window to upper right"
+GetActiveWinGeo
+
+echo "-> moving window to upper right"
 xdotool windowmove $window 0 0
+echo ""
+
+GetActiveWinGeo
