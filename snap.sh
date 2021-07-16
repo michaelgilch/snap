526931f7c6b16a99b84b576065eeb57b02021e97
Michael Gilchrist <michaelgilch@gmail.com>
2021-07-16 04:18:40 -0400

    Add basic movements to left and right

--- snap.sh
+++ snap.sh
@@ -2,6 +2,15 @@
 #
 # Window Snapping Utility for Openbox
 
+# These should be calculated dynamically later.
+screen_x_start=0
+screen_y_start=26
+screen_x_total=3840
+screen_y_total=1054
+
+monitor1_x_start=0
+monitor2_x_start=1920
+
 # Stores the _SNAP_STATE xprop value in WINDOW_STATE
 # If no _SNAP_STATE property is found, stores "N/A"
 # Example: _SNAP_STATE(STRING) = "test"
@@ -27,16 +36,28 @@ function dev_test() {
 WINDOW=$(xdotool getactivewindow)
 echo "Window: $WINDOW"
 
-dev_test
+#dev_test
 
 get_window_state
 echo "State: $WINDOW_STATE"
 
 case $WINDOW_STATE in
     "N/A" )
-        echo "Window has not yet been snapped. Starting at [0,0]"
+        echo "Window has not yet been snapped. Starting at Quadrant [0,0]"
         ;;
     "test" )
         echo "Window has been found!"
         ;;
 esac
+
+cmd=$1
+if [ $cmd == 'l' ]; then
+    let new_x=$screen_x_start
+    let new_y=$screen_y_start
+elif [ $cmd == 'r' ]; then
+    let new_x=$screen_x_total/4
+    let new_y=$screen_y_start
+fi
+
+xdotool windowmove $WINDOW $new_x $new_y
+
